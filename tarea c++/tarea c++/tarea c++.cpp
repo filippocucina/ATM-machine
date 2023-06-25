5722dc28f3c5a884e1fb5cd00e40b208aa08163b
Filippo <filippocucina5@gmail.com>
2023-06-25 18:42:43 -0400

    ATM machine code. This code will get better after sending the homework, running out of time!

--- tarea c++/tarea c++/tarea c++.cpp
+++ tarea c++/tarea c++/tarea c++.cpp
@@ -3,14 +3,13 @@
 #include <ctime>
 #include <thread>
 #include <chrono>
-#define TIEMPO_DE_ESPERA 4
+#define TIEMPO_DE_ESPERA 3
 
 
 int main(int argc, char *argv[])
 {
     std::string nombre_cliente;
     int PIN_tarjeta;
-    int contador = 0;
     int saldo_inicial = 50000; 
     int saldo_resultado;
     int saldo_restante;
@@ -20,45 +19,55 @@ int main(int argc, char *argv[])
 
 
     std::cout << "\tBienvenido al Banco Fermin Toro\n" << std::endl;
-    std::cout << "\tPor favor Ingrese su numero de PIN. No puedo ser mayor y menor que 4\n" << std::endl;
+    std::cout << "\tPor favor Ingrese su numero de PIN\n" << std::endl;
 
-    //Ciclo While para determinar si el usuario ingresó un numero mayor o menor que 4
-    //Si es igual a 4, el bucle se cierra y continua el menu del Cajero Automatico
-    while (std::cin >> PIN_tarjeta) {
-        contador += 1;
-
-
-        if (PIN_tarjeta > 4) {
-            std::cout << "\tError en el PIN. Por favor vuelvelo a intentar\n" << std::endl;
-            break;
-            return 0;
-        }
-
-        if (PIN_tarjeta < 4) {
-            std::cout << "\tError en el PIN. Por favor vuelvelo a intentar\n" << std::endl;
-            break;
-            return 0;
-        }
-
-    }
-
-
-    /*  
-    if (PIN_tarjeta > sizeof(6)) {
-        std::cout << "\tError en el PIN. Por favor vuelvelo a intentar\n" << std::endl;
-        return 0;
-
-    } else if (PIN_tarjeta < sizeof(4)) {
-        std::cout << "\tError en el PIN. Por favor vuelvelo a intentar\n" << std::endl;
-        return 0;
-
-    } else {
-        std::cout << "\tPIN correcto\n" << std::endl;
-    }*/
-
+    std::cin >> PIN_tarjeta;
+ 
     std::cout << "\t...Cargando\n" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(TIEMPO_DE_ESPERA));
     
+    std::cout << "\tTiene un saldo de: " << saldo_inicial << std::endl;
+    std::cout << "\tQue desea hacer?\n" << std::endl;
+    std::cout << "\t1. Ingresar dinero en cuenta\n" << std::endl;
+    std::cout << "\t2. Retirar dinero de la cuenta\n" << std::endl;
+    std::cout << "\t3. Salir\n" << std::endl;
+    std::cout << "\tOpcion: \n" << std::endl;
+    std::cin >> opcion;
+
+    switch (opcion) {
+        case 1: std::cout << "\tCuanto dinero desea Ingresar en la cuenta?\n" << std::endl;
+                std::cin >> dinero_a_depositar;
+                saldo_resultado = saldo_inicial + dinero_a_depositar;
+                std::this_thread::sleep_for(std::chrono::seconds(TIEMPO_DE_ESPERA));
+                std::cout << "\tSaldo resultado es: " << saldo_resultado << std::endl;
+                break;
+
+        case 2: std::cout << "\tCuando dinero desea retirar de la cuenta?\n" << std::endl;
+                std::cin >> dinero_a_retirar;
+
+                if (dinero_a_retirar > 50000) {
+                    saldo_restante = saldo_inicial - dinero_a_retirar;
+                    std::this_thread::sleep_for(std::chrono::seconds(TIEMPO_DE_ESPERA));
+                    std::cout << "\tSu saldo es igual a 0. Gracias por venir!\n" << std::endl;
+                    break;
+                }
+
+                if (dinero_a_retirar == 0) {
+                    std::cout << "\tPor favor ingrese un monto que no sea igual a 0\n" << std::endl;
+                    std::this_thread::sleep_for(std::chrono::seconds(TIEMPO_DE_ESPERA));
+                    break;
+                }
+
+                saldo_restante = saldo_inicial - dinero_a_retirar;
+                std::this_thread::sleep_for(std::chrono::seconds(TIEMPO_DE_ESPERA));
+
+                std::cout << "\tLe queda en su cuenta: " << saldo_restante << std::endl;
+                return 0;
+
+
+        case 3: std::cout << "\tGracias por venir!\n" << std::endl;
+                break;
+    }
 
     return 0;
 }
